fe1b3d6acd3b4f74f351d6f9204e34a87e90b24d
Katrolli <ennio_cano@live.com>
2022-11-09 06:27:12 -0800

    pchar and string

--- functionts.c
+++ functionts.c
@@ -1,14 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-
+/**
+ * p_char -main function
+ * @char_list: first parameter
+ * Return: returns the sum
+ */
 int p_char(va_list char_list)
 {
 	char c = va_arg(char_list, int);
+
 	putchar(c);
 	return (1);
 }
-
+/**
+ * p_string -main function
+ * @str_list: first parameter
+ * Return: returns the sum
+ */
 int p_string(va_list str_list)
 {
 	int i = 0;
@@ -22,40 +31,31 @@ int p_string(va_list str_list)
 	}
 	return (i);
 }
-
+/**
+ * _printf -main function
+ * @format: first parameter
+ * Return: returns the sum
+ */
 int _printf(const char *format, ...)
 {
-	int i = 0, count = 0;
-	int j;
-
-	my_struct f_struct[] =
-	{
-		{"c", p_char},
-		{"s", p_string},
-		{NULL, NULL}
-	};
-
+	int i = 0, count = 0, j;
+	my_struct f_struct[] = {{"c", p_char}, {"s", p_string}, {NULL, NULL}};
 	va_list args_list;
 
 	if (format == NULL)
 		return (-1);
 	va_start(args_list, format);
-
-	while(format != NULL && format[i] != '\0')
+	for (i = 0; format != NULL && format[i] != '\0'; i++)
 	{
 		if (format[i] == '%' && format[i + 1] != '%')
 		{
-			j = 0;
-			while (f_struct[j].type != NULL)
+			for (j = 0; f_struct[j].type != NULL; j++)
 			{
-				if (format [i + 1] == f_struct[j].type[0])
-				{
+				if (format[i + 1] == f_struct[j].type[0])
 					count += f_struct[j].f(args_list);
-				}
-				j++;
 			}
 			if (format[i + 1] != '\0' && format[i + 1] != 'c' && format[i + 1] != 's')
-			{	
+			{
 				putchar(format[i]);
 				putchar(format[i + 1]);
 				count += 2;
@@ -75,7 +75,6 @@ int _printf(const char *format, ...)
 			putchar(format[i]);
 			count += 1;
 		}
-		i++;
 	}
 	va_end(args_list);
 	return (count);
