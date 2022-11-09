f245e96a00c1de0e556ded8d42544d5e60da7693
Katrolli <ennio_cano@live.com>
2022-11-09 03:57:35 -0800

    fixing more printf

--- main.c
+++ main.c
@@ -9,7 +9,17 @@
 int main(void)
 {
 
-	_printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
-    return (0);
+
+	int len, len2;
+
+	len = _printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+	len2 = printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	return (0);
 }
